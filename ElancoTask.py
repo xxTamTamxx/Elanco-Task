0e0321be0625f2ddfecca5c694bfec49ff992c60
xxTamTamxx <112004275+xxTamTamxx@users.noreply.github.com>
2022-11-27 16:24:27 +0000

    Add files via upload

---
+++ ElancoTask.py
@@ -0,0 +1,6 @@
+import requests
+import json
+res= requests.get('https://engineering-task.elancoapps.com/api/raw')
+print(res.json())
+with open('ElancoRaww','w') as json_file:
+    json.dump(res.json(), json_file)
